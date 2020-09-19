03e113ddbd81463feb3905f25ecd473b9df18ee4
AmeyGohil <amey23399@gmail.com>
2020-09-19 21:42:41 +0530

    query portal added

--- new/assets/utils/test.php
+++ new/assets/utils/test.php
@@ -16,7 +16,7 @@ function send_email($from_mail, $from_name, $to_email, $to_name, $subject, $mail
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->SMTPDebug = 3;                      // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication

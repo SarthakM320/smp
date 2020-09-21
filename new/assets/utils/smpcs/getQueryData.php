b7e3724663dd2dfa7219772f1fbe8ed3c396206c
AmeyGohil <amey23399@gmail.com>
2020-09-22 03:33:26 +0530

    query portal added

--- new/assets/utils/smpcs/getQueryData.php
+++ new/assets/utils/smpcs/getQueryData.php
@@ -4,7 +4,7 @@ function getQueries(){
     try {
         $link = linkToSMP();
         $handle = null;
-        $sql="SELECT *,id as DT_RowId FROM `queries` ORDER BY `id` ASC WHERE `answer`='0'";
+        $sql="SELECT *,id as DT_RowId FROM `queries` ORDER BY `id` ASC WHERE `answered`='0'";
         $handle=$link->prepare($sql);
         $handle->execute();
         $result=$handle->fetchAll(PDO::FETCH_ASSOC);
